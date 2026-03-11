316e0376101b5cd2d52a4a0072ade437953f19dd
Isabelle <155624058+Isabelle36@users.noreply.github.com>
2026-03-11 22:07:37 +0530

    initialized clerk and added 404 page with error boundary

---
+++ app/Components/Heroo.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+function Heroo() {
+    // throw new Error("simulated error");
+  return (
+    <div className='bg-amber-200 justify-center items-center flex w-full min-h-screen '>
+      
+<h1 className='text-4xl text-black font-bold text-center flex items-center justify-center'>
+  LogMyClass
+  </h1>
+
+    </div>
+  )
+}
+
+export default Heroo
